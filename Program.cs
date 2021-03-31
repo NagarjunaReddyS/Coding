f5b010e41d5709b7654548d12e92276a2906aded
NagarjunareddyS <nagarjunareddy.sr@hotmail.com>
2021-03-31 12:48:08 +0530

    First Migration Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Test_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "nagarjuna";
+            char[] charvalue= str.ToCharArray();
+            string s = string.Empty;
+
+            for(int i= charvalue.Length - 1; i <= 0; i--)
+            {
+                s = s + charvalue[i];
+            }
+            Console.WriteLine(s);
+           
+        }
+    }
+
+   
+}
